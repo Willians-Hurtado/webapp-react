e42b5f9d7cccafe8b07fa1f2480fd7722d238d66
Willians-Hurtado <willianshurtado5@gmail.com>
2025-04-16 16:21:13 +0200

    Implement movie fetching and display on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,55 @@
 
+import { useState, useEffect, use } from 'react';
+
 
 export default function HomePage() {
 
+    const [movies, setMovies] = useState([]);
 
+    useEffect(() => {
+        fetch('http://localhost:3000/api/v1/movies')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setMovies(data);
+            });
+    }, []);
 
     return (
         <div>
             <h1>Home Page</h1>
             <p>Welcome to the home page!</p>
+            <section>
+                <div className="container">
+
+                    <div className="row row-cols-sm-2 row-cols-lg-4">
+
+                        {
+                            movies.map(movie => (
+                                <div className='card' key={movie.id}>
+                                    <img src={`http://localhost:3000/images/${movie.image}`} className="card-img-top" alt="" />
+                                    <div className="card-body">
+                                        <h2>{movie.title}</h2>
+                                        <p>{movie.director}</p>
+                                        <p className="card-text">{movie.abstract}</p>
+
+                                    </div>
+
+                                </div>
+                            ))
+
+                        }
+
+                    </div>
+
+                </div>
+            </section>
+
+
+
+
+
+
 
         </div>
     );
