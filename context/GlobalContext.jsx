3168283cf3c9fd22ca21363a830802008bb42b91
Willians-Hurtado <willianshurtado5@gmail.com>
2025-04-22 17:52:33 +0200

    Integrate GlobalContext provider in App component and create GlobalContext file

---
+++ context/GlobalContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+
+const GlobalContext = createContext()
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default GlobalContext
--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "./layout/DefaultLayout.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import SingleMovie from "./pages/SingleMovie.jsx"
+import GlobalContext from "../context/GlobalContext.jsx"
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultLayout}>
+    <GlobalContext.Provider value={{}}>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
 
-          <Route path="/" Component={HomePage} />
-          <Route path="/movies/:id" Component={SingleMovie} />
+            <Route path="/" Component={HomePage} />
+            <Route path="/movies/:id" Component={SingleMovie} />
 
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
